7707c30d87d16b8e0e7a1e05858bf36610ff41a0
aumi364 <aumi.asif@gmail.com>
2021-09-02 13:26:10 +0600

    fixes for deploying

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ##Quiz App
 Created with react (cra).
-#####Dependencies
+##### Dependencies
 react-hook-form, react-icons, bootstrap
 
-#####Features
+##### Features
 *  Admin  - Admin can view, add and edit questions. Admin can also view answer but can't edit or add answer
 *  General User  - User can view, add and edit answers. Users  do not have any access to admin apages and components
   
--- src/Pages/Utils/DummyQuestions.js
+++ src/Pages/Utils/DummyQuestions.js
@@ -9,29 +9,4 @@ export const DummyQuestions = [
     question: "What is your pet's name ?",
     answers: [],
   },
-  {
-    id: 3,
-    question: "What is your pet's name ?",
-    answers: [],
-  },
-  {
-    id: 4,
-    question: "What is your pet's name ?",
-    answers: [],
-  },
-  {
-    id: 5,
-    question: "What is your pet's name ?",
-    answers: [],
-  },
-  {
-    id: 6,
-    question: "What is your pet's name ?",
-    answers: [],
-  },
-  {
-    id: 7,
-    question: "What is your pet's name ?",
-    answers: [],
-  },
 ];
